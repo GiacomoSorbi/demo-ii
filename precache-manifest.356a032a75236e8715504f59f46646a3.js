000b55b897add2082b2c53db6dde72f3e87bf8f7
Giacomo Sorbi <g.sorbi@gmail.com>
2020-05-23 15:07:54 +0100

    Updates

---
+++ precache-manifest.356a032a75236e8715504f59f46646a3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27caf8cbed5d94d70fe85ab1f145828b",
+    "url": "/demo-ii/index.html"
+  },
+  {
+    "revision": "131355ea1a2d9660fbc1",
+    "url": "/demo-ii/static/css/main.eb0609d2.chunk.css"
+  },
+  {
+    "revision": "67bb7230cd6c4c1c5015",
+    "url": "/demo-ii/static/js/2.606c3547.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/demo-ii/static/js/2.606c3547.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "131355ea1a2d9660fbc1",
+    "url": "/demo-ii/static/js/main.d7acda47.chunk.js"
+  },
+  {
+    "revision": "cb35f30414a32d9425c0",
+    "url": "/demo-ii/static/js/runtime-main.3cd3d75a.js"
+  }
+]);
